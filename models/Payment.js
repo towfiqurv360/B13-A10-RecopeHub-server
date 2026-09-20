c024ac54b63f355b17a0dc9b95f5125a8f76c9b1
Md Towfiqur Rahman <140777602+towfiqurv360@users.noreply.github.com>
2026-09-20 12:36:19 +0600

    Add Payment model schema

---
+++ models/Payment.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const paymentSchema = new mongoose.Schema(
+  {
+    userEmail: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    amount: { type: Number, required: true },
+    recipeId: { type: mongoose.Schema.Types.ObjectId, ref: "Recipe" },
+    paymentType: { type: String, enum: ["recipe", "premium"], required: true }, 
+    transactionId: { type: String, required: true },
+    paymentStatus: { type: String, required: true },
+    paidAt: { type: Date, default: Date.now },
+  }
+);
+
+export default mongoose.model("Payment", paymentSchema);
